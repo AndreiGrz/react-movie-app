b1c3bc45f429af735e4f215699957f377caa95b6
agurzun <andrei.gurzun@tss-yonder.com>
2024-02-12 18:17:12 +0200

    creating dummy components for each route [using react-router-dom]

---
+++ movie-app/src/components/header/Header.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import './header.scss';
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return <div className="header">
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav className="menu-items">
+            <div className="left">
+                <Link to="/" className="nav-link">
+                    Home
+                </Link>
+                <Link to="/login" className="nav-link">
+                    Login
+                </Link>
+                <Link to="/register" className="nav-link">
+                    Register
+                </Link>
+            </div>
+            <div className="right">
+                <Nav.Link href="#register">Welcone, Guest</Nav.Link>
+            </div>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>;
+};
+
+export default Header
